0e5688390f1b85189116f2553c71891ac9f5e609
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-11-13 14:33:06 -0500

    added event listeners to JS

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('cityInput');
+    const searchButton = document.querySelector('.searchButton');
+    const historyContainer = document.querySelector('aside');
+    const cityName = document.getElementById('cityName');
+    const temp = document.getElementById('temp');
+    const wind = document.getElementById('wind');
+    const humidity = document.getElementById('humidity');
+    const forecastContainer = document.getElementById('forecastContainer');
+
+});
