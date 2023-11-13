38daca5662d1e12f8e626205491dd56afd62e641
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-11-13 14:39:01 -0500

    Function to get weather data for a given city

--- Assets/script.js
+++ Assets/script.js
@@ -10,6 +10,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // OpenWeatherMap API key
   const apiKey = '4e6e807ee59834126e8fdbcfad716167';
+  let historyLoaded = false; // Flag to check if history has been loaded
+
+// Function to get weather data for a given city
+function getWeather(city) {
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+    // Fetch current weather data
+    fetch(currentWeatherUrl)
+        .then(response => response.json())
+        .then(data => {
+            updateCurrentWeather(data);
+            if (!isCityInHistory(city)) {
+                addCityToHistory(city);
+                saveHistoryToStorage();
+            }
+        })
+        .catch(error => console.error('Error fetching current weather data:', error));
+
+    // Fetch forecast data
+    fetch(forecastUrl)
+        .then(response => response.json())
+        .then(data => {
+            updateForecast(data);
+        })
+        .catch(error => console.error('Error fetching forecast data:', error));
+}
+
 
 
 
